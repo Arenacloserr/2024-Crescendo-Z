7aa5bf2f27325fd58a612a2141db6a1c677c0bce
Arenacloserr <162641455+Arenacloserr@users.noreply.github.com>
2024-11-07 20:27:52 -0500

    Update RevvingCommand.java

--- src/main/java/frc/robot/commands/RevvingCommand.java
+++ src/main/java/frc/robot/commands/RevvingCommand.java
@@ -4,7 +4,7 @@
 
 package frc.robot.commands;
 
-// import frc.robot.subsystems.Shooter.*;
+import frc.robot.subsystems.Shooter.Shooter;
 
 import frc.robot.subsystems.leds.LEDs;
 
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class RevvingCommand extends Command {
     LEDs leds = LEDs.getInstance();
+    Shooter shooter = Shooter.getInstance();
     double RPM;
     double MAX;
 
@@ -27,16 +28,19 @@ public class RevvingCommand extends Command {
     @Override
     public void initialize() {
         leds.setTopColorHSV(0, 0, 0);
+        leds.setBottomColorHSV(0, 0, 0);
         leds.setOutput();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        this.RPM = shooter.currentRPM();
+        this.MAX = shooter.currentMAX();
         // Hue is 0-180
         for (int i = 0; i < leds.getLength(); i++) {
             if (this.RPM >= (this.MAX*(((double) i*(100/((double) leds.getLength()/1)))/100))) { // edit length if needed
-                leds.setHSVIndex(i, 0, 0, 0);
+                leds.setHSVIndex(i, 100, 100, 100);
             }
             else {
                 break;
